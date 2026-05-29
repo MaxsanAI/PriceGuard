54128322155cc509b45f9eb9e27651906fb4c8b6
MaxsanAI <maxsansamurai@gmail.com>
2026-05-29 05:50:05 +0200

    Fixing MainActivity: Removing problematic imports

--- android/app/src/main/java/com/priceguard/app/MainActivity.java
+++ android/app/src/main/java/com/priceguard/app/MainActivity.java
@@ -1,14 +1,5 @@
-package com.your.app.name; // Ovo ostaje tvoj paket
+package com.priceguard.app;
 
 import com.getcapacitor.BridgeActivity;
-import android.os.Bundle;
-import com.capacitorcommunity.barcodescanner.BarcodeScanner; // OVO JE KLJUČNO
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Registruj plugin ručno ako auto-inicijalizacija zeza
-        registerPlugin(com.capacitorcommunity.barcodescanner.BarcodeScanner.class);
-    }
-}
+public class MainActivity extends BridgeActivity {}
