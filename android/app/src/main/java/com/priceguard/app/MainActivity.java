75453fc5eb3ef8eab32cb8d53002663457f51a81
MaxsanAI <maxsansamurai@gmail.com>
2026-05-29 05:06:03 +0200

    Manual plugin registration for Android 9

--- android/app/src/main/java/com/priceguard/app/MainActivity.java
+++ android/app/src/main/java/com/priceguard/app/MainActivity.java
@@ -1,5 +1,14 @@
-package com.priceguard.app;
+package com.your.app.name; // Ovo ostaje tvoj paket
 
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+import com.capacitorcommunity.barcodescanner.BarcodeScanner; // OVO JE KLJUČNO
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Registruj plugin ručno ako auto-inicijalizacija zeza
+        registerPlugin(com.capacitorcommunity.barcodescanner.BarcodeScanner.class);
+    }
+}
